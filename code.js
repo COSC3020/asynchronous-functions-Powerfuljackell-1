14461a4d2c92d21713a1620cfa1ffecbd209c89e
powerfuljackell <johnsonjakel@aol.com>
2024-12-09 14:19:20 -0700

    updated code.js

--- code.js
+++ code.js
@@ -1,15 +1,22 @@
-function nmPMR(arr, key, cb) {
+/*function nmPMR(arr, key, cb) {
     var Parallel = require('paralleljs'),
     p = new Parallel(arr);
     global.process.env.key = key;
     p.map(function(i) { return i == global.process.env.key; })
     .reduce(function(d) { return d[0] + d[1]; })
     .then(cb);
+}*/
+
+async function nmAsync(arr, key, cb) {
+    array = arr.map((i)=>i == key).reduce((accum, next)=> accum + next)
+    cb(array);
 }
+
 
 /*
 let arr = Array.from('aaaaaaabcdef1hijk')
 console.log(arr)
 var logResult = function (){console.log(arguments[0])}
-nmPMR(arr, "a", logResult)
+nmAsync(arr, "a", logResult)
 */
+
